522003db4ce9d0971d770eeff08a1074ff4d5b07
Trista Liu <jliu2217@gmail.com>
2024-06-12 19:26:59 +1000

    update front url in orderController

--- food-backend/controllers/orderController.js
+++ food-backend/controllers/orderController.js
@@ -5,7 +5,7 @@ import Stripe from "stripe";
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 // Placing User Order for Frontend
 const placeOrder = async (req, res) => {
-  const frontend_url = "http://localhost:5173";
+  const frontend_url = "https://food-app-nine-blue.vercel.app/";
   try {
     const newOrder = new orderModel({
       userId: req.body.userId,
